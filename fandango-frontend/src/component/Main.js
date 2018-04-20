9f9d4a779235c4c50eebdc563c463c388421f049
Rajiv Yadav <rajivramesh.yadav@sjsu.edu>
2018-04-19 20:31:12 -0700

    header4

--- fandango-frontend/src/component/Main.js
+++ fandango-frontend/src/component/Main.js
@@ -2,12 +2,12 @@ import React from 'react';
 import {Switch,Route} from 'react-router-dom';
 import movies from './movies';
 import ticketboxoffice from './ticketboxoffice';
-import ticketboxoffice from './headers';
+import headers from './headers';
 
 const Main = () => (
     <Switch>
         <Route exact path = '/movies' component={movies}/>
-        <Route exact path = '/' component={Headers}/>
+        <Route exact path = '/' component={headers}/>
         <Route exact path = '/transaction/ticketboxoffice' component={ticketboxoffice} />
     </Switch>
 )
