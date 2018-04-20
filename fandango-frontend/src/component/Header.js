9943b04b368a9ec7ae7b57b936213d17a4ef0794
Rajiv Yadav <rajivramesh.yadav@sjsu.edu>
2018-04-19 19:53:19 -0700

    Test Commit

---
+++ fandango-frontend/src/component/Header.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default class Hedaer extends React.Component {
+    render() {
+        return (
+            <div>HELLO</div>
+        );
+    }
+}
--- fandango-frontend/src/component/signin.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class signin extends React.Component {
-    render() {
-        return (
-            <div>HELLO</div>
-        );
-    }
-}
