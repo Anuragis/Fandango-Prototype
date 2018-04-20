cd54a880c2bb06dac80a4a3785d7dcd82d662e38
Anurag Shinde <anuragshinde@Anurags-MacBook-Pro.local>
2018-04-20 00:52:29 -0700

    grouping routes

--- fandango-frontend/src/component/Main.js
+++ fandango-frontend/src/component/Main.js
@@ -7,9 +7,9 @@ import moviedetails from './moviedetails';
 
 const Main = () => (
     <Switch>
-        <Route exact path = '/movies' component={movies}/>
         <Route exact path = '/' component={headers}/>
         <Route exact path = '/transaction/ticketboxoffice' component={ticketboxoffice} />
+        <Route exact path = '/movies' component={movies}/>
         <Route exact path ='/moviedetails/:mid' component={moviedetails}/>
     </Switch>
 )
