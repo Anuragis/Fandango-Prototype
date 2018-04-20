4bd894e5139b72d0c62d30c69b308c88e506c144
Rajiv Yadav <rajivramesh.yadav@sjsu.edu>
2018-04-19 19:55:39 -0700

    test

--- fandango-frontend/src/component/Header.js
+++ fandango-frontend/src/component/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 
-export default class Hedaer extends React.Component {
+export default class Header extends React.Component {
     render() {
         return (
             <div>HELLO</div>
