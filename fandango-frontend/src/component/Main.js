07ead830b0bf4b8a066b6e4150b0145ed9e2bc0d
ameytotawar <ameytotawar@gmail.com>
2018-04-19 20:19:25 -0700

    css

--- fandango-frontend/src/component/Main.js
+++ fandango-frontend/src/component/Main.js
@@ -5,6 +5,7 @@ import movies from './movies';
 const Main = () => (
     <Switch>
         <Route exact path = '/movies' component={movies}/>
+        <Route exact path = '/transaction/ticketboxoffice' component={ticketboxoffice} />
     </Switch>
 )
 
---
+++ fandango-frontend/src/component/ticketboxoffice.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class ticketboxoffice extends React.Component {
+    render() {
+        return (
+            <div class="commonContainer"> 
+                <div id="logo">
+                    <a href="http://www.fandango.com/" title="Click to go to Fandango homepage">Fandango Home</a>
+                </div>
+            </div>
+        );
+    }
+}
