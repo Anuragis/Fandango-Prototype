a223b93ea811a49f65dd83c730f97b6ba5d7f7d7
Gaurav Savner <gaurav.savner@sjsu.edu>
2018-04-21 19:06:52 -0700

    Added empty component

--- fandango-frontend/src/component/Main.js
+++ fandango-frontend/src/component/Main.js
@@ -6,15 +6,19 @@ import userhome from './userhome';
 import moviedetails from './moviedetails';
 import seatpicker from './seatpicker';
 import admin from './admin';
+import signin from './signin';
+import signup from './signup';
 
 const Main = () => (
     <Switch>
         <Route exact path = '/' component={userhome}/>
-        <Route exact path = '/transaction/ticketboxoffice' component={ticketboxoffice} />
-        <Route exact path = '/transaction/seatpicker' component={seatpicker} />
-        <Route exact path = '/movies' component={movies}/>
-        <Route exact path ='/moviedetails/:mid' component={moviedetails}/>
-        <Route exact path = '/admin' component={admin}/>
+        <Route path = '/transaction/ticketboxoffice' component={ticketboxoffice} />
+        <Route path = '/transaction/seatpicker' component={seatpicker} />
+        <Route path = '/movies' component={movies}/>
+        <Route path ='/moviedetails/:mid' component={moviedetails}/>
+        <Route path = '/admin' component={admin}/>
+        <Route path = '/signin' component={signin}/>
+        <Route path = '/signup' component={signup}/>
     </Switch>
 )
 
---
+++ fandango-frontend/src/component/signin.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+
+export default class Signin extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+
+  render() {
+    return(
+      <div>Sign In Page</div>
+    );
+  }
+}
