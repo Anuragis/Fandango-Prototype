7ee0df1d80706e1581094fefb4027cf2501756d4
Anurag Shinde <anuragshinde@Anurags-MacBook-Pro.local>
2018-04-27 17:51:35 -0700

    adding booking module

--- node-backend/app/controllers/bookings.js
+++ node-backend/app/controllers/bookings.js
@@ -74,3 +74,14 @@ module.exports.findBookingsByHallname = function(req,res,next){
     })
     
 }
+
+
+module.exports.getAllBookings = function(req,res,next){
+   
+    bookingsModel.find({status: "open"}, function(err, booking) {
+        if (err)
+            throw err;
+        res.send(booking);
+    })
+    
+}
