aa9dac112b70b376971795e7cfe5643022c46d01
Anurag Shinde <anuragshinde@Anurags-MacBook-Pro.local>
2018-05-01 04:02:46 -0700

    adding movie halls

--- node-backend/app/controllers/moviehalls.js
+++ node-backend/app/controllers/moviehalls.js
@@ -95,12 +95,9 @@ module.exports.updateMovieHall=function(req,res,next){
         if (err)
             throw err;
 
-<<<<<<< HEAD
-=======
         console.log("update Hall",result);
         console.log("update Hall screens",result.screens[0].movieTimings);
 
->>>>>>> 8f8bb35a82452644fd1aaea70e4d5f82045d8777
         let seatArray = ['M9','M8','M7','M6','M5','M4','M3','M2','M1','L9','L8','L7','L6','L5','L4','L3','L2','L1','K9','K8','K7','K6','K5','K4','K3','K2','K1','J10','J9','J8','J7','J6','J5','J4','J3','J2','J1','I11','I10','I9','I8','I7','I6','I5','I4','I3','I2','I1','H13','H12','H11','H10','H9','H8','H7','H6','H5','H4','H3','H2','H1','G14','G13','G12','G11','G10','G9','G8','G7','G6','G5','G4','G3','G2','G1','F15','F14','F13','F12','F11','F10','F9','F8','F7','F6','F5','F4','F3','F2','F1','E16','E15','E14','E13','E12','E11','E10','E9','E8','E7','E6','E5','E4','E3','E2','E1','D14','D13','D12','D11','D10','D9','D8','D7','D6','D5','D4','D3','D2','D1','C17','C16','C15','C14','C13','C12','C11','C10','C9','C8','C7','C6','C5','C4','C3','C2','C1','B17','B16','B15','B14','B13','B12','B11','B10','B9','B8','B7','B6','B5','B4','B3','B2','B1','A15','A14','A13','A12','A11','A10','A9','A8','A7','A6','A5','A4','A3','A2','A1'];
         result.screens = result.screens.map((screen) => {
             let tempTimings = screen.movieTimings;
